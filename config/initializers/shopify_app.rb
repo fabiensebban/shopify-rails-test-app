6446a365a275c6e8fb43e4c6c767ccce2c155af4
Fabien <fa.sebban@gmail.com>
2018-03-10 19:04:47 +0000

    First commit with shopify app

---
+++ config/initializers/shopify_app.rb
@@ -0,0 +1,9 @@
+ShopifyApp.configure do |config|
+  config.application_name = "My Shopify App"
+  config.api_key = "cb4b3eec5fde263f20ae774b39c18af9"
+  config.secret = "540fd3b6a53684f0e5b3a284f1abce4e"
+  config.scope = "read_orders, read_products"
+  config.embedded_app = true
+  config.after_authenticate_job = false
+  config.session_repository = Shop
+end
